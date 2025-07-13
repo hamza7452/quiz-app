99d917f7ec3fb2e6d4e40c10a1de95442dafb01e
hamza khan <mhkhamza989@gmail.com>
2025-07-13 15:49:57 +0530

    latest javascript

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const questions = [
   {
-    question: "which is largest animal in the world?",
+    question: "which is largest ani mal in the world?",
     answers: [
       { text: "Elephant", correct: false },
       { text: "Blue Whale", correct: true },
@@ -45,3 +45,88 @@ const questions = [
     ]
   }
 ]
+
+const questionelement = document.getElementById("question");
+const answerbutton = document.getElementById("answer-buttons");
+const nextbutton = document.getElementById("next-btn");
+
+let currentquestionindex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentquestionindex = 0;
+  score = 0;
+  nextbutton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  let currentquestion = questions[currentquestionindex];
+  let questionno = currentquestionindex + 1;
+  questionelement.innerHTML = questionno + ".   " + currentquestion.question;
+
+  answerbutton.innerHTML = "";
+
+  currentquestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerbutton.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  })
+}
+
+function selectAnswer(e) {
+  const selectedbtn = e.target;
+  const iscorrect = selectedbtn.dataset.correct === "true";
+  if (iscorrect) {
+    selectedbtn.classList.add("correct");
+    score++;
+  }
+  else {
+    selectedbtn.classList.add("incorrect");
+  }
+  Array.from(answerbutton.children).forEach(button => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextbutton.style.display = "block";
+}
+
+function resetstate() {
+  nextbutton.style.display = "none";
+  answerbutton.innerHTML = "";
+}
+
+function handlenextquestion() {
+  currentquestionindex++;
+  if (currentquestionindex < questions.length) {
+    showQuestion();
+  }
+  else {
+    showscore();
+  }
+}
+
+nextbutton.addEventListener("click", () => {
+  if (currentquestionindex < questions.length) {
+    handlenextquestion();
+  } else {
+    startQuiz();
+  }
+})
+
+function showscore() {
+  resetstate();
+  questionelement.innerHTML = `you scored ${score} out of ${questions.length}!`;
+  nextbutton.innerHTML = "Play Again";
+  nextbutton.style.display = "block";
+}
+
+startQuiz();
+
